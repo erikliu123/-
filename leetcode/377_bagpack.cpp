5c389cfdc6702d53d2951d9c4da0974dfa5e25be
erikliu123 <1171045702@qq.com>
2021-04-25 21:11:46 +0800

    LC377 带记忆的暴力搜索pass

--- leetcode/377_bagpack.cpp
+++ leetcode/377_bagpack.cpp
@@ -24,6 +24,7 @@ github
 class Solution {
 public:
     //unordered_map<int, int> cnt;
+    vector<int> dp;
     int ans;
     /*
     测试用例1： [5,1,8] 24
@@ -31,46 +32,30 @@ public:
     [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300,310,320,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,480,490,500,510,520,530,540,550,560,570,580,590,600,610,620,630,640,650,660,670,680,690,700,710,720,730,740,750,760,770,780,790,800,810,820,830,840,850,860,870,880,890,900,910,920,930,940,950,960,970,980,990,111]
 999
     */
-    void DFS(vector<int>& nums, int begin, int cur_sum, int target, vector<int> &tmp)
+    int DFS(vector<int>& nums, int target, vector<int> &tmp)
     {
-        if(cur_sum == target )
+        
+        if(dp[target] >= 0) return dp[target];
+        int res=0;
+        for(int i=0; i<nums.size() ; i++)
         {
-            int t=1;
-            long long w=1;
-           //NOTICE:计算[1,1,2,2]的种类， 4!/(2!*2!)
-            for(int i=1; i<tmp.size() || t>1; i++){
-                if(i<tmp.size())
-                    w*=(i+1);
-                if(i<tmp.size() && tmp[i]==tmp[i-1]) {
-                    t++;
-                    w/=t;
-                }
-                else{
-                    t=1; 
-                }
-            }
-            ans=ans+w;
-            
-            return ;
+            //tmp.push_back(nums[i]);
+            if(target >= nums[i])
+                res+=DFS(nums,  target-nums[i], tmp);
+            //tmp.pop_back();
         }
-        else if(cur_sum > target){
-            return ;
-        }
-        for(int i=begin; i<nums.size() && cur_sum+nums[i]<=target; i++)
-        {
-            tmp.push_back(nums[i]);
-            DFS(nums, i, cur_sum+nums[i], target, tmp);
-            tmp.pop_back();
-        }
-        return ;
+        dp[target]=res;
+        return res;
     }
     int combinationSum4(vector<int>& nums, int target) {
         ans=0;
         vector<int> temp;
-
+        dp=vector<int>(target+1, -1);
+        dp[0] = 1;//很重要
         sort(nums.begin(), nums.end());
-        DFS(nums, 0, 0, target, temp);
-        return ans;
+        DFS(nums, target, temp);
+        //return ans;
+        return dp[target];
     }
 };
 
@@ -82,7 +67,7 @@ int main()
     370,380,390,400,410,420,430,440,450,460,470,480,490,500,510,520,530,540,550,560,570,580,590,600,610,620,630,640,650,660,670,680,690,700,710,720,730,740,750,
     760,770,780,790,800,810,820,830,840,850,860,870,880,890,900,910,920,930,940,950,960,970,980,990,111};
     int target=999;
-    cout<<a.combinationSum4(small_test, 4)<<endl;
+    cout<<a.combinationSum4(small_test, 4)<<endl;//[1,2,3] 7
     cout<<a.combinationSum4(bigtest, 999)<<endl;
     return 0;
 }
